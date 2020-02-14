ab22a0bd6167bfef81a8a3f861a1f7171462c991
Wellington Guimaraes <wellguimaraes@gmail.com>
2020-02-14 19:45:40 -0300

    first usable version

---
+++ README.md
@@ -0,0 +1,75 @@
+# MobX React Router Utils
+Routing utils to use with RouterStore (from `mobx-react-router`) on your MobX stores.
+
+## Install it
+```
+yarn add mobx-react-router-utils
+``` 
+
+## Use it
+```typescript
+import { computedRouteParam, setRoutingStore } from 'mobx-react-router-utils'
+
+// Set the reference to your routing store before any usage
+// You ony need to this once in your application 
+setRoutingStore(routingStore)
+
+const routes = {
+  search: '/search',
+  citySearch: '/regions/:region'
+}
+
+class DemoSearchStore {
+    city = computedRouteParam('city', {
+      patterns: [routes.citySearch],
+    })
+    
+    checkIn = computedRouteParam('checkIn', {
+      // Define the matching patterns
+      patterns: [routes.search, routes.citySearch],
+      
+      // Set a parsing fn to transform from string
+      parse: _parseDate,
+      
+      // Set a formatting fn to transform into string
+      format: _formatDate, 
+
+      // It'll return the default value in case it's 
+      // not present on the route or with an empty value
+      defaultValue: 'temecula'
+    })
+    
+    checkOut = computedRouteParam('checkOut', {
+      patterns: [routes.search, routes.citySearch],
+      parse: _parseDate,
+      format: _formatDate,
+    })
+    
+    setRegion = (city: Maybe<string>) => {
+      // For updating the current route, push() and replace() methods are available:
+      // - push: will add the new url to the browsing history
+      // - replace: will replace the current url on the browsing history
+      this.city.push(city, {
+        // enforce a route pattern if needed, otherwise, it will use the 
+        // current location (if matches the config) or first pattern available
+        pattern: !city && routes.search,
+        
+        // cleanup all other params (default: false)
+        cleanParams: true,  
+              
+        // OR cleanup only selected params
+        cleanParams: [   
+          this.checkIn, 
+          this.checkOut
+        ]
+      })
+    }
+    
+    setPeriod = (checkIn: Maybe<Date>, checkOut: Maybe<Date>) => {
+      this.checkIn.push(checkIn)
+      this.checkOut.replace(checkOut)
+    }
+
+    // ...
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,150 @@
+import omit from 'lodash/omit'
+import omitBy from 'lodash/omitBy'
+import pick from 'lodash/pick'
+import pickBy from 'lodash/pickBy'
+import memoizee from 'memoizee'
+import { computed } from 'mobx'
+import { RouterStore } from 'mobx-react-router'
+import { IComputedValue } from 'mobx/lib/internal'
+import { compile, Key, match, pathToRegexp } from 'path-to-regexp'
+import { parse as parseQueryString, stringify as toQueryString } from 'query-string'
+
+type ComputedRouteOptions<T = string> = {
+  patterns: string[]
+  parse?: (it: string) => T
+  format?: (it: any) => string
+  defaultValue?: T
+}
+
+type SetRouteParamOptions = {
+  pattern?: string | undefined | null | false
+  cleanParams?: boolean | ComputedRouteParam<any>[]
+}
+
+type ComputedRouteParam<T> = IComputedValue<T> & { push: RouteParamSetter; replace: RouteParamSetter; _paramName: string }
+
+type RouteParamSetter = (newValue: any, options?: SetRouteParamOptions) => void
+
+let _emptyObject = {}
+let _routingStore: RouterStore
+
+export const setRoutingStore = (routingStore: RouterStore) => (_routingStore = routingStore)
+
+const getLocationQuery = () => parseQueryString(window.location.search)
+
+const ensureLeft = (str: string, prefix: string) => {
+  if (str.startsWith(prefix)) return str
+  return prefix + str
+}
+
+const getComputedRouteParams = memoizee((routePatterns: string[]) => {
+  const matchers = routePatterns.map(it => match(it, { decode: decodeURIComponent }))
+
+  return computed(() => {
+    let pathParams = {}
+    let queryParams = parseQueryString(_routingStore.location.search)
+
+    for (let match of matchers) {
+      let matchResult: false | { params: any } = match(_routingStore.location.pathname)
+
+      if (matchResult === false) continue
+
+      pathParams = matchResult.params
+    }
+
+    const pathParamsIsEmpty = Object.keys(pathParams).length === 0
+    const queryParamsIsEmpty = Object.keys(queryParams).length === 0
+
+    return pathParamsIsEmpty && queryParamsIsEmpty
+      ? _emptyObject
+      : {
+          ...pathParams,
+          ...queryParams,
+        }
+  })
+})
+
+export const interpolateUrl = (pattern: string, params: any) => {
+  const pathKeys = [] as Key[]
+
+  pathToRegexp(pattern, pathKeys)
+  const toPath = compile(pattern)
+  const namesOfPathKeys = pathKeys.map(it => it.name)
+
+  const queryParams = omit(params, namesOfPathKeys)
+  const pathParams = pick(params, namesOfPathKeys)
+
+  const query = toQueryString({
+    ...omitBy(queryParams, value => !value && value !== false),
+    ...pickBy(getLocationQuery(), (value, key: string) => key.startsWith('utm')),
+  })
+
+  return toPath(pathParams) + (query && ensureLeft(query, '?'))
+}
+
+export const computedRouteParam = <T>(
+  paramName: string,
+  { patterns: routePatterns, parse, format, defaultValue }: ComputedRouteOptions<T>
+) => {
+  const routeParams = getComputedRouteParams(routePatterns)
+  const routePatternsWithRegex = routePatterns.map(it => ({ pattern: it, regex: pathToRegexp(it) }))
+
+  const computedValue = computed(() => {
+    const params: any = routeParams.get()
+    const paramValue = params && params[paramName]
+
+    let typedParamValue
+
+    try {
+      typedParamValue = paramValue && parse ? parse(paramValue) : paramValue
+    } catch (err) {
+      console.error(err)
+    }
+
+    const paramValueIsEmpty = typedParamValue === undefined || typedParamValue === '' || typedParamValue === null
+
+    return paramValueIsEmpty ? defaultValue : typedParamValue
+  }) as ComputedRouteParam<T>
+
+  computedValue._paramName = paramName
+
+  const setRouteParamWith = (setter: (url: string) => void) => (newValue: any, options = {} as SetRouteParamOptions) => {
+    let currentPattern
+
+    if (options && options.pattern) {
+      currentPattern = { pattern: options.pattern }
+    } else {
+      switch (routePatterns.length) {
+        case 0:
+          currentPattern = { pattern: _routingStore.location.pathname }
+          break
+
+        case 1:
+          currentPattern = routePatternsWithRegex[0]
+          break
+
+        default:
+          currentPattern = routePatternsWithRegex.find(it => it.regex.test(_routingStore.location.pathname))
+          break
+      }
+    }
+
+    const params: any = routeParams.get()
+    const newValueFormatted = format && newValue ? format(newValue) : newValue
+    const currentParams = Array.isArray(options.cleanParams)
+      ? omit(
+          params,
+          options.cleanParams.map(it => it._paramName)
+        )
+      : options.cleanParams === true
+      ? {}
+      : params
+
+    currentPattern && setter(interpolateUrl(currentPattern!.pattern, { ...currentParams, [paramName]: newValueFormatted }))
+  }
+
+  computedValue.push = setRouteParamWith(_routingStore.history.push)
+  computedValue.replace = setRouteParamWith(_routingStore.history.replace)
+
+  return computedValue
+}
